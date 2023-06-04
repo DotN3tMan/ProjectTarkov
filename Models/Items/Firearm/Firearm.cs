1440ea220e9ff8c6ef4d9a2f19c7e5b35772033a
DotN3tMan <b1zne7po4ta@gmail.com>
2023-06-04 10:27:02 +0500

    add new models and refact

--- Models/Items/Firearm/Firearm.cs
+++ Models/Items/Firearm/Firearm.cs
@@ -1,65 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Text.Json.Serialization;
 
 namespace Models.Items.Firearm
 {
-    internal sealed class Firearm
+    public sealed class Firearm
     {
+        [JsonPropertyName("_id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("shortName")]
+        public string ShortName { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+
+        [JsonPropertyName("weight")]
+        public float Weight { get; set; }
+
+        [JsonPropertyName("maxStack")]
+        public int MaxStack { get; set; }
+
+        [JsonPropertyName("rarity")]
+        public string Rarity { get; set; }
+
+        [JsonPropertyName("_modified")]
+        public string Modified { get; set; }
+
+        [JsonPropertyName("_kind")]
+        public string Kind { get; set; }
+
         /// <summary>
         /// The type of the firearm, e.g. primary, secondary
         /// </summary>
-        public string type {get; set; }
+        [JsonPropertyName("type")]
+        public string Type {get; set; }
+
         /// <summary>
         /// The weapon class, e.g. assaultCarbine, pistol,  smg
         /// </summary>
-        public string klass {get; set; }
+        [JsonPropertyName("class")]
+        public string Class {get; set; }
+
         /// <summary>
         /// The weapon caliber
         /// </summary>
-        public string caliber {get; set; }
+        [JsonPropertyName("caliber")]
+        public string Caliber {get; set; }
+
         /// <summary>
         /// The rate of fire of the weapon
         /// </summary>
-        public int rof {get; set; }
+        [JsonPropertyName("rof")]
+        public int Rof {get; set; }
+
         /// <summary>
         /// Number of rounds per burst, if available
         /// </summary>
-        public int burstRounds {get; set; }
+        [JsonPropertyName("burstRounds")]
+        public int BurstRounds {get; set; }
+
         /// <summary>
         /// The action type e.g. gas, recoil, bolt
         /// </summary>
-        public string action {get; set; }
+        [JsonPropertyName("action")]
+        public string Action {get; set; }
+
         /// <summary>
         /// The fire modes of the weapon
         /// </summary>
-        public string[] modes {get; set; }
+        [JsonPropertyName("modes")]
+        public string[] Modes {get; set; }
+
         /// <summary>
         /// The muzzle velocity in meters per second
         /// </summary>
-        public float velocity {get; set; }
+        [JsonPropertyName("velocity")]
+        public float Velocity {get; set; }
+
         /// <summary>
         /// The maximum effective range of the weapon
         /// </summary>
-        public float effectiveDist {get; set; }
+        [JsonPropertyName("effectiveDist")]
+        public float EffectiveDist {get; set; }
+
         /// <summary>
         /// The base ergonomics of the firearm
         /// </summary>
-        public float ergonomics {get; set; }
+        [JsonPropertyName("ergonomics")]
+        public float Ergonomics {get; set; }
+
         /// <summary>
         /// Indicates if the butstock is fold or rectractable
         /// </summary>
-        public bool foldRectractable {get; set; }
+        [JsonPropertyName("foldRectractable")]
+        public bool FoldRectractable {get; set; }
+
         /// <summary>
         /// The base vertical recoil of the weapon
         /// </summary>
-        public float recoilVertical {get; set; }
+        [JsonPropertyName("recoilVertical")]
+        public float RecoilVertical {get; set; }
+
         /// <summary>
         /// The base horizontal recoil of the weapon
         /// </summary>
-        public float recoilHorizontal {get; set; }
+        [JsonPropertyName("recoilHorizontal")]
+        public float RecoilHorizontal {get; set; }
+
         /// <summary>
         /// A map of the modification slots
         /// </summary>
-        public Slot[] slots {get; set; }
+        [JsonPropertyName("slots")]
+        public Dictionary<string, Slot> Slots {get; set; }
     }
 }
--- Models/Items/Firearm/Slot.cs
+++ Models/Items/Firearm/Slot.cs
@@ -1,14 +1,18 @@
-﻿namespace Models.Items.Firearm
+﻿using System.Text.Json.Serialization;
+
+namespace Models.Items.Firearm
 {
-    internal sealed class Slot
+    public sealed class Slot
     {
         /// <summary>
         /// A map of compatible item IDs categorized by kind
         /// </summary>
-        public object filter { get; set; }
+        [JsonPropertyName("filter")]
+        public Dictionary<string, string[]> Filter { get; set; }
         /// <summary>
         /// Indicates whether the slot is required for an operational state
         /// </summary>
-        public bool required { get; set; }
+        [JsonPropertyName("required")]
+        public bool Required { get; set; }
     }
 }
