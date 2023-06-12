2d5621f59d25ec70d11602290ee069647dacc04e
DotN3tMan <b1zne7po4ta@gmail.com>
2023-06-12 14:31:41 +0500

    reset

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,11 +1,1 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Collections.Generic;
-using System.IO;
-using Models.Items.Firearm;
-
-var jsonStr = File.ReadAllText("firearm.json");
-
-var obj = JsonSerializer.Deserialize<Firearm>(jsonStr);
-
-Console.ReadKey();
+﻿
